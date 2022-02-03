29c9b2f930615378358728e8e21d1e8b5e4063bf
nicstim <sashapskov60@gmail.com>
2022-02-03 22:42:00 +0300

    csrf

--- game/urls.py
+++ game/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
+from django.views.decorators.csrf import csrf_exempt
 from game.views import Index
 
 app_name = "game"
 
 urlpatterns = [
     path("", Index.as_view(), name="index"),
-    path("score/", Index.end_game, name="end_game"),
+    path("score/", csrf_exempt(Index.end_game), name="end_game"),
 ]
